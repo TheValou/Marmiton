295aacd8d9dbf0cb35a1bf59996fbb683180c7c4
TheValou <wurfl.valentin@gmail.com>
2016-10-19 12:55:53 +0200

    test sql

--- MODEL/Connector.php
+++ MODEL/Connector.php
@@ -3,12 +3,13 @@
 class Connector
 {
 
-    private $PDO_HOST = 'localhost';
-    private $PDO_DBBASE = 'marmiton';
-    private $PDO_USER = 'root';
-    private $PDO_PW = '';
+    private $PDO_HOST = 'eu-cdbr-west-01.cleardb.com';
+    private $PDO_DBBASE = 'heroku_2c626774a6f246b';
+    private $PDO_USER = 'bacb1bfb30a150';
+    private $PDO_PW = '3a84ccab';
     private static $_instance = NULL;    
 
+//user name - password - HOST - database
 //mysql://bacb1bfb30a150:3a84ccab@eu-cdbr-west-01.cleardb.com/heroku_2c626774a6f246b?reconnect=true
 
     private function __construct()
